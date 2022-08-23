e99ae48c9b4345f1adaac4ac0c39e266f0c0dfcd
Eduardotmartins <eduardo.toyoshima@gmail.com>
2022-08-23 09:23:24 +0900

    exercicios PythonBrasil

---
+++ pythonbrasil/Estrutura-sequencial/conversaomc.py
@@ -0,0 +1,9 @@
+# Faça um Programa que converta metros para centímetros.
+# peça para o promp o valor a ser inserido em metro
+metro = int(input("Insira os metros: "))
+
+# formula para conversao de metro para centimetro
+conversao = metro * 100
+
+# imprima o valor da conversao demonstrando em centimetros
+print(f"{conversao} Centimetros")
