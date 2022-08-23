b60ce67ca7b98a4f0990d5f3ed80ccbf87ceeb8b
Eduardotmartins <eduardo.toyoshima@gmail.com>
2022-08-23 09:47:36 +0900

    exercicios python brasil

--- pythonbrasil/Estrutura-sequencial/raiodecirculo.py
+++ pythonbrasil/Estrutura-sequencial/raiodecirculo.py
@@ -1,10 +1,10 @@
 # Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
 
 #faça o promp pedir o raio de um circulo
-x = int(input("Insira o medida do raio do circulo: "))
+x = float(input("Insira o medida do raio do circulo: "))
 
 # formula da area do circulo
-area = (3.14 * x) * 4
+area = 3.14 * (x * x)
 
 #imprima os valores para o promp
-print(f"A area do circulo é: {area}")
+print("A area do circulo é: {:.2f}".format (area))
